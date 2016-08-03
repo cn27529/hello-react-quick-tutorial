2acfc3e8a9c06ac05107647295d3b19240063f81
Bruce.Y.Huang (twmkplmis.twdb01.NeweggTW) 62258 <bh96@buyabs.corp>
2016-08-03 13:41:33 +0800

    20160803a

--- app/level-04_first-component/TodoApp.js
+++ app/level-04_first-component/TodoApp.js
@@ -3,6 +3,20 @@ class TodoApp extends React.Component {
   // 2. 必須實作 render 方法：
   //    透過該方法回傳的元素，讓 React 瞭解要如何繪製該元件在頁面上
   render() {
-    return <div>TodoApp</div>;
+    return <h1>TodoApp</h1>;
   }
 }
+
+/*
+var TodoApp = React.createClass({
+  render: function(){
+    return(
+      <h1>TodoApp</h1>
+    );
+  }
+});
+*/
+
+// 3. 將元件類別 (TodoApp) 定義在 window.App 下：
+//    這可以讓其他 JS 檔使用該元件類別
+window.App.TodoApp = TodoApp;
